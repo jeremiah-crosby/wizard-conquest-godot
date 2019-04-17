8308315808d9cebf4ef98e3af5db32f4a26b6ef5
Jeremiah <jeremiahcrosby@gmail.com>
2019-04-16 22:17:49 -0400

    Fix diagonal movement

--- characters/Player.gd
+++ characters/Player.gd
@@ -1,19 +1,20 @@
 extends KinematicBody2D
 
 const BASE_SPEED = 200
-var velocity = Vector2()
 
 func _physics_process(delta):
+	var velocity = Vector2()
 	if Input.is_action_pressed("play_move_left"):
 		velocity.x = -1
-	elif Input.is_action_pressed("play_move_right"):
+	
+	if Input.is_action_pressed("play_move_right"):
 		velocity.x = 1
-	elif Input.is_action_pressed("play_move_down"):
+		
+	if Input.is_action_pressed("play_move_down"):
 		velocity.y = 1
-	elif Input.is_action_pressed("play_move_up"):
+	
+	if Input.is_action_pressed("play_move_up"):
 		velocity.y = -1
-	else:
-		velocity.x = 0
-		velocity.y = 0
+	
 		
 	self.move_and_slide(velocity.normalized() * BASE_SPEED)
